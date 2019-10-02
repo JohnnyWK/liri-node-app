0305c2d4f398d7b3cf46535b871a3e8e2305fcb9
Johnny Kreitzman <kreitzka@gmail.com>
2019-10-02 16:50:39 -0500

    set up initial files and modules.  created vars

---
+++ liri.js
@@ -0,0 +1,10 @@
+var axios = require("axios");
+
+var request = require("request");
+var fs = require("fs");
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+var userOption = process.argv[2]; 
+var inputParameter = process.argv[3];
