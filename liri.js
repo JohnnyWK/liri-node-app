7555f029c2fb88d54ead3fd653a5a8526f3ebec1
Johnny Kreitzman <kreitzka@gmail.com>
2019-10-03 20:55:01 -0500

    spotify search working

--- liri.js
+++ liri.js
@@ -22,6 +22,8 @@ function RunLiri(command, searchTerm){
             return searchSpotify(searchTerm);
         case 'do-what-it-says':
             return runDoIt();
+        case 'concert-this':
+            return searchBands(searchTerm);
         default:
             return console.log("Command not good!", command)
     }
@@ -37,8 +39,66 @@ function searchOMDB(term){
     console.log("Searching OMDB for %s.", term)
 }
 function searchSpotify(term){
-    console.log("Searching Spotify for %s.", term)
+    // console.log("Searching Spotify for %s.", term)
+    if (inputParameter === undefined) {
+        inputParameter = "The Sign"; //default Song
+    }
+    spotify.search(
+        {
+            type: "track",
+            query: inputParameter
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log("**********SONG INFO*********");
+                fs.appendFileSync("log.txt", "**********SONG INFO*********\n");
+                console.log(i);
+                fs.appendFileSync("log.txt", i +"\n");
+                console.log("Song name: " + songs[i].name);
+                fs.appendFileSync("log.txt", "song name: " + songs[i].name +"\n");
+                console.log("Preview song: " + songs[i].preview_url);
+                fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url +"\n");
+                console.log("Album: " + songs[i].album.name);
+                fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
+                console.log("Artist(s): " + songs[i].artists[0].name);
+                fs.appendFileSync("log.txt", "artist(s): " + songs[i].artists[0].name + "\n");
+                console.log("*****************************");  
+                fs.appendFileSync("log.txt", "*****************************\n");
+             }
+        }
+    );
 }
+function searchBands(term){
+    // console.log("Searching BandsInTown for %s.", term)
+    var queryUrl = "https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp";
+    request(queryUrl, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+        var results = JSON.parse(body);
+        for (var i = 0; i < results.length; i++) {  
+            console.log("**********EVENT INFO*********");  
+            fs.appendFileSync("log.txt", "**********EVENT INFO*********\n");
+            console.log(i);
+            fs.appendFileSync("log.txt", i+"\n");
+            console.log("Venue: " + results[i].venue.name);
+            fs.appendFileSync("log.txt", "Venue: " + results[i].venue.name+"\n");
+            console.log("Location: " +  results[i].venue.city);
+            fs.appendFileSync("log.txt", "Location: " +  results[i].venue.city+"\n");
+            console.log("Date: " +  results[i].datetime);
+            fs.appendFileSync("log.txt", "Date: " +  results[i].datetime+"\n");
+            console.log("*****************************");
+            fs.appendFileSync("log.txt", "*****************************"+"\n");
+        }
+    } else{
+      console.log('Error occurred.');
+    }
+});}
+
 function runDoIt(){
     fs.readFile("./textFiles/random.txt", "utf8", function(err, data){
         if(err) throw err;
